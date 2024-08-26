422bc2c546d92f4d40f4e368c07cc0f366d30445
Ayu0317mi <n0217ayumi@gmail.com>
2024-08-26 14:54:26 +1000

    convert page.tsx and person-table.tsx to server components

--- src/app/generic/page.tsx
+++ src/app/generic/page.tsx
@@ -1,51 +1,21 @@
-'use client';
+'use server';
 
-import { z } from "zod"
-import { useForm } from "react-hook-form"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import GenericDialog from "./GenericDialog"
-import { Table, TableBody, TableCell, TableHead, TableRow } from "@/components/ui/table";
 import { getPerson } from "./generic-actions";
-import { useState, useEffect } from "react";
-import PersonForm from "./person-form";
-import PersonTabel from "./person-table";
-import { personFormSchema } from "./form-schema";
+import PersonTable from "./person-table";
 
-type PersonFormData = z.infer<typeof personFormSchema>;
 
-export default function ExampleUsage() {
-  const addUser = async (data: PersonFormData) => {
-    // Server action to add user
-    console.log('Adding user:', data)
-  }
-
-  const editUser = async (data: PersonFormData) => {
-    // Server action to edit user
-    console.log('Editing user:', data)
-  }
-
-  const existingUser = { firstname: "John", lastname: "Doe" }
+export default async function ExampleUsage() {
+  const persons = await getPerson();
 
   return (
       <div>
-        <div className="space-y-4">
-          <GenericDialog
-            FormComponent={PersonForm}
-            addAction={addUser}
-            editAction={editUser}
-          />
-          <GenericDialog
-            FormComponent={PersonForm}
-            object={existingUser}
-            addAction={addUser}
-            editAction={editUser}
-          />
-        </div>
-        <div>
-          <PersonTabel />
-        </div>
+          <div>
+          <PersonTable persons={persons ?? []} />;
+          </div>
       </div>
-    )
+  )
 }
+
+
+
+
--- src/app/generic/person-table.tsx
+++ src/app/generic/person-table.tsx
@@ -1,65 +1,71 @@
-'use client';
+'use server';
 
-import { z } from "zod"
-import { useForm } from "react-hook-form"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
-import GenericDialog from "./GenericDialog"
+import { z } from "zod";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import GenericDialog from "./GenericDialog";
 import { Table, TableBody, TableCell, TableHead, TableRow } from "@/components/ui/table";
 import { getPerson } from "./generic-actions";
-import { useState, useEffect } from "react";
 import PersonForm from "./person-form";
 import { personFormSchema } from "./form-schema";
+
+type PersonFormData = z.infer<typeof personFormSchema>;
 
 interface Person {
     id: number;
     firstname: string;
     lastname: string;
-  }
-  
-  const PersonTable = () => {
-    const [persons, setPersons] = useState<Person[]>([]);
-  
-    useEffect(() => {
-      getPerson().then((data) => {
-        if (data) {
-          setPersons(data);
-        }
-      });
-    }, []);
-  
-    const mockData: Person[] = [];
-  
-    const addPersonToMockData = (person: Person) => {
-      mockData.push(person);
-      setPersons([...mockData]);
-    };
-  
-    // Example usage of addPersonToMockData
-    const handleAddPerson = () => {
-      const newPerson: Person = { id: 1, firstname: 'John', lastname: 'Doe' };
-      addPersonToMockData(newPerson);
-    };
-    
-    return (
-        <Table>
-        <TableHead>
-            <TableRow>
-            <TableCell>First Name</TableCell>
-            <TableCell>Last Name</TableCell>
-            </TableRow>
-        </TableHead>
-        <TableBody>
-            {persons.map((person) => (
-            <TableRow key={person.id}>
-                <TableCell>{person.firstname}</TableCell>
-                <TableCell>{person.lastname}</TableCell>
-            </TableRow>
-            ))}
-        </TableBody>
-        </Table>
-    );
-    }
+}
 
-    export default PersonTable;
+interface PersonTableProps {
+    persons: Person[];
+}
+
+const PersonTable = ({ persons }: PersonTableProps) => {
+    const addUser = async (data: PersonFormData) => {
+        console.log('Adding user:', data);
+    };
+
+    const editUser = async (data: PersonFormData) => {
+        // Server action to edit user
+        console.log('Editing user:', data);
+    };
+
+    const existingUser = { firstname: "John", lastname: "Doe" };
+
+    return (
+        <div>
+            <GenericDialog
+                FormComponent={PersonForm}
+                addAction={addUser}
+                editAction={editUser}
+            />
+
+            <GenericDialog
+                FormComponent={PersonForm}
+                object={existingUser}
+                addAction={addUser}
+                editAction={editUser}
+            />
+
+            <Table>            
+                <TableHead>
+                    <TableRow>
+                        <TableCell>First Name</TableCell>
+                        <TableCell>Last Name</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {persons.map((person) => (
+                        <TableRow key={person.id}>
+                            <TableCell>{person.firstname}</TableCell>
+                            <TableCell>{person.lastname}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </div>
+    );
+};
+
+export default PersonTable;
